5ac15d0776c13bf9362ac70d2665a67fc8443cd5
joaquin-labrador <joaquinlabrador1010@gmail.com>
2023-05-24 14:22:50 -0300

    Add @Builder class for Errors response

--- src/main/java/com/api/security/Exceptions/ApiErrorResponse.java
+++ src/main/java/com/api/security/Exceptions/ApiErrorResponse.java
@@ -1,2 +1,17 @@
-package com.api.security.Exceptions;public class ApiErrorResponse {
+package com.api.security.Exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ApiErrorResponse {
+    LocalDateTime timestamp;  //2021-08-18T17:00:00.000
+    private String message;
+    private int status;
+
 }
