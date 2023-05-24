9b1de7057038ef7ad6c5e77cb2c44f8af0192291
joaquin-labrador <joaquinlabrador1010@gmail.com>
2023-05-24 14:24:20 -0300

    Add class to throw BadCredentials in to auth logic

--- src/main/java/com/api/security/Exceptions/BadCredentials.java
+++ src/main/java/com/api/security/Exceptions/BadCredentials.java
@@ -1,2 +1,7 @@
-package com.api.security.Exceptions;public class BadCredentials {
+package com.api.security.Exceptions;
+
+public class BadCredentials extends RuntimeException {
+    public BadCredentials(String message) {
+        super(message);
+    }
 }
